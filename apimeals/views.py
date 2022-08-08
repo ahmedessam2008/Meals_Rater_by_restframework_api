8330d85ed727d2686004122fdc0562e537fefe53
ahmedessam2008 <ahmessam@yahoo.com>
2022-08-08 18:21:14 +0200

    Setup ang basics

---
+++ apimeals/views.py
@@ -0,0 +1,16 @@
+from rest_framework import request, status, viewsets
+from .models import Meal, Rating
+from .serializers import MealSerializer, RatingSerializer
+
+
+
+class MealViewSet(viewsets.ModelViewSet):
+    queryset = Meal.objects.all()
+    serializer_class = MealSerializer
+
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+ 
