643caa31ff1f4328f11ddad4720c723df7bfe7f8
ahmedessam2008 <ahmessam@yahoo.com>
2022-08-08 21:56:53 +0200

    Write logic

--- meals_rater/urls.py
+++ meals_rater/urls.py
@@ -2,7 +2,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apimeals.urls')),
+    path('tokenrequest/', obtain_auth_token)
 ]
