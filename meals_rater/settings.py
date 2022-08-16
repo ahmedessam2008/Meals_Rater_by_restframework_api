7c6bc4a0b00ddd0fd4fc75ff31db47d86aa54a78
ahmedessam2008 <ahmessam@yahoo.com>
2022-08-16 21:33:54 +0200

    new chance to deploy

--- meals_rater/settings.py
+++ meals_rater/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,7 +139,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'meals_rater/static'),
 ]
-
+django_heroku.settings(locals())
 
 # Media Settings 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
